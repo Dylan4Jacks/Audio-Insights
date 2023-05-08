d338082950fa53fe1a146510e055f3504c378cda
Dylan Jack <48197729+Dylan4Jacks@users.noreply.github.com>
2023-05-08 23:08:24 +1000

    Slim 2

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ app = FastAPI()
 #html directory
 app.mount("/public", StaticFiles(directory="public"), name="public")
 templates = Jinja2Templates(directory="public/views")
+
 # Load the saved model
-imported = tf.saved_model.load('./models/GUI_model')
+# imported = tf.saved_model.load('./models/GUI_model')
+
 @app.get("/")
 async def root():
     return RedirectResponse(url="/index")
@@ -47,20 +49,20 @@ def base64_to_wav(base64_string):
 
 @app.post("/make_post")
 async def make_request(audiodata: AudioData):
-  audio_string = tf.constant(audiodata.audio, dtype=tf.string)
-  encode_string = tf.io.decode_base64(audio_string)
-  audio_tensor, sample_rate = tf.audio.decode_wav(encode_string)
+  # audio_string = tf.constant(audiodata.audio, dtype=tf.string)
+  # encode_string = tf.io.decode_base64(audio_string)
+  # audio_tensor, sample_rate = tf.audio.decode_wav(encode_string)
   # Get the prediction dictionary
-  prediction = imported(audio_tensor)
+  # prediction = imported(audio_tensor)
 
-  # Get the predicted class name and probability
-  #predicted_class_name = prediction['class_names'][0].numpy().decode('utf-8')
-  predicted_class_id = prediction['class_ids'][0].numpy()
-  predicted_probability = 100 * prediction['predictions'][0][0].numpy()
-  label_names = ['Piano','Voice', 'Trumpet', 'Saxophone', 'Organ' ,'Clarinent' ,'Acoutic Guitar' ,'Violin', 'Flute', 'Electric Guitar', 'Cello']
-  # Print the predicted class name and probability
-  print("Predicted class:", label_names[predicted_class_id])
-  print("Predicted probability:", '{:.2f}%'.format(predicted_probability))
+  # # Get the predicted class name and probability
+  # #predicted_class_name = prediction['class_names'][0].numpy().decode('utf-8')
+  # predicted_class_id = prediction['class_ids'][0].numpy()
+  # predicted_probability = 100 * prediction['predictions'][0][0].numpy()
+  # label_names = ['Piano','Voice', 'Trumpet', 'Saxophone', 'Organ' ,'Clarinent' ,'Acoutic Guitar' ,'Violin', 'Flute', 'Electric Guitar', 'Cello']
+  # # Print the predicted class name and probability
+  # print("Predicted class:", label_names[predicted_class_id])
+  # print("Predicted probability:", '{:.2f}%'.format(predicted_probability))
 
-  print("prediction")
-  return str("Predicted probability:", '{:.2f}%'.format(predicted_probability))
+  # print("prediction")
+  return "Endpoint currently Yields Error due to Base64 -> Wav file conversion issues"  #str("Predicted probability:", '{:.2f}%'.format(predicted_probability))
